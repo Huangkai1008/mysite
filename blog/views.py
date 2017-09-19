ba1e618d3b9fb7c3319677c6c94546c34723d90e
huangkai <18778335525@163.com>
2017-09-19 22:13:33 +0800

    添加分页

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # 如果页码不是整数那么传到第一页
+        posts = paginator.page(1)
+    except EmptyPage:
+        # 如果页码为空那么传到结果的最后一页
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, post, year, month, day):
