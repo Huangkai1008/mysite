0439526127682f389edcb68d16b83d6810f8495d
huangkai <18778335525@163.com>
2017-09-20 16:15:37 +0800

    添加评论系统

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,17 @@
 from django import forms
+from .models import Comment
 
 
+# E-mail分享表单
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
+
+
+# 评论表单
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
